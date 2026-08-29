3111086597022c27072629d854a4766ec50b588b
Matheus Siqueira <matheuspontessiqueira@gmail.com>
2026-08-29 02:06:06 -0300

    .

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,25 +4,31 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 import os
+import django
 from django.http import JsonResponse
+from django.contrib.staticfiles.finders import get_finders
 
 def debug_static(request):
-    static_root = str(settings.STATIC_ROOT)
-    existe = os.path.isdir(settings.STATIC_ROOT)
-    arquivos = []
-    if existe:
-        for root, dirs, files in os.walk(settings.STATIC_ROOT):
-            for f in files[:5]:
-                arquivos.append(os.path.relpath(os.path.join(root, f), settings.STATIC_ROOT))
-            if len(arquivos) >= 20:
-                break
+    admin_static_path = os.path.join(os.path.dirname(django.__file__), 'contrib', 'admin', 'static')
+
+    finders_info = []
+    total_encontrados = 0
+    for finder in get_finders():
+        paths = list(finder.list(None))
+        total_encontrados += len(paths)
+        finders_info.append({
+            'finder': finder.__class__.__name__,
+            'quantidade_encontrada': len(paths),
+        })
+
     return JsonResponse({
-        'STATIC_ROOT': static_root,
-        'pasta_existe': existe,
-        'total_arquivos_amostra': len(arquivos),
-        'exemplos': arquivos,
-        'STATIC_URL': settings.STATIC_URL,
-        'DEBUG': settings.DEBUG,
+        'STATIC_ROOT': str(settings.STATIC_ROOT),
+        'STATICFILES_FINDERS': settings.STATICFILES_FINDERS,
+        'STATICFILES_DIRS': getattr(settings, 'STATICFILES_DIRS', None),
+        'admin_static_existe_no_pacote_django': os.path.isdir(admin_static_path),
+        'admin_static_path': admin_static_path,
+        'total_arquivos_encontrados_pelos_finders': total_encontrados,
+        'detalhe_por_finder': finders_info,
     })
 
 urlpatterns = [
