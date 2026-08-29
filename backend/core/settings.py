715c7ed945f21edb3b16d4045dd075908e33953a
Matheus Siqueira <matheuspontessiqueira@gmail.com>
2026-08-29 01:28:30 -0300

    .

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -130,7 +130,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
